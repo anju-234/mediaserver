15ebfdc11611b9474d3c53a4559d09a4ceb232d3
anju-234 <anjum23561@gmail.com>
2025-01-02 14:49:24 +0900

    changed all

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const JSONServer=require('json-server')
-const MPServer=JSONServer.create()
-const router=JSONServer.router('db.json')
-const middleware=JSONServer.defaults()
+const JSONServer=require('json-server')// Importing json-server
+const MPServer=JSONServer.create()// Creating the server instance
+const router=JSONServer.router('db.json')// Defining the router, which uses 'db.json' as the database
+const middleware=JSONServer.defaults()// Applying default middlewares
 const PORT=3000 || process.env.PORT
 MPServer.use(middleware)
 MPServer.use(router)
